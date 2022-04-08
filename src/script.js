4df4306a1c2c60c7d1ae1e5532ba9e8d4f91a265
dashabasya <dashabasya29@gmail.com>
2022-04-08 12:31:26 +0200

    add HW5-DOM

---
+++ src/script.js
@@ -0,0 +1,12 @@
+document.querySelector('.btn').addEventListener('click', () => {
+    const userName = document.getElementById('username').value;
+    const userPassword = document.getElementById('userPassword').value;
+    document.querySelector('.out').innerHTML = 'Name: ' + userName + ' / Password: ' + userPassword;
+
+
+    localStorage.setItem('user', JSON.stringify({name: userName, password: userPassword}));
+});
+
+
+
+
