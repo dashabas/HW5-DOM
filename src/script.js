cf5b8e7a51bbf119005570ca7e5fcd5045402882
dashabasya <dashabasya29@gmail.com>
2022-04-09 17:03:30 +0200

    add second option

--- src/script.js
+++ src/script.js
@@ -8,5 +8,25 @@ document.querySelector('.btn').addEventListener('click', () => {
 });
 
 
+// One more option
+// const userName = document.querySelector('#username');
+// const userPassword = document.querySelector('#userPassword');
+// const out = document.querySelector('.out');
+// const btn = document.querySelector('.btn');
+// btn.addEventListener('click', () => {
+//     out.innerHTML = 'Name: ' + userName.value + ' / Password: ' + userPassword.value;
+//
+//     localStorage.setItem('user', JSON.stringify({name: userName.value, password: userPassword.value}));
+// });
+
+
+
+
+
+
+
+
+
+
 
 
